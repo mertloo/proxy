ba0cd853624198de2f5cfcea7cdbf33f7ef5200e
mertloo <mert.472114271@gmail.com>
2018-08-12 03:49:55 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#./proxy -nextserver 172.96.198.61:16812 -password woshimima
 if [[ $1 == "socks5" ]];then
 	./proxy -next 0.0.0.0:20002 -password woshimima -timeout 2 -goro 5
 elif [[ $1 == "ssocks" ]];then
