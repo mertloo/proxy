918e5235ae5df905fd485884907883c490ecc4cf
mertloo <mert@mert-laptop>
2018-08-13 02:37:28 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ $1 == "socks5" ]];then
-	./proxy -next 0.0.0.0:20002 -password woshimima -timeout 2 -goro 5
+	./proxy -next 0.0.0.0:20002 -password woshimima -goro 5
 elif [[ $1 == "ssocks" ]];then
-	./proxy -proto ssocks -addr 0.0.0.0:20002 -password woshimima -timeout 2 -goro 4
+	./proxy -proto ssocks -addr 0.0.0.0:20002 -password woshimima -goro 5
 else
 	echo 'Usage: start.sh <socks5|ssocks>'
 fi
